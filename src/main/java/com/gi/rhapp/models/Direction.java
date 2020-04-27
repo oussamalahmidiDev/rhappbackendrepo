8acc970fca5e47e63e4a682723c7e61c0536c9a7
Vvoox <khalil.daoulat@gmail.com>
2020-04-27 16:40:25 +0000

    -some models

--- src/main/java/com/gi/rhapp/models/Direction.java
+++ src/main/java/com/gi/rhapp/models/Direction.java
@@ -1,4 +1,31 @@
 package com.gi.rhapp.models;
 
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Data
 public class Direction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+
+    @CreationTimestamp
+    private Date dateCreation;
+
+    @UpdateTimestamp
+    private Date dateUpdate;
 }
--- src/main/java/com/gi/rhapp/models/TypeConge.java
+++ src/main/java/com/gi/rhapp/models/TypeConge.java
@@ -1,4 +1,23 @@
 package com.gi.rhapp.models;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Data
 public class TypeConge {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String typeConge;
 }
