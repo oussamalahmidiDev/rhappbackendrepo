e172989d10294421ca4e60354baf5526e370723a
OussamaDev <oussama.lahmidi@icloud.com>
2020-04-27 03:21:09 +0000

    Deployed to Heroku

---
+++ src/main/java/com/gi/rhapp/RhappApplication.java
@@ -0,0 +1,13 @@
+package com.gi.rhapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RhappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gi/rhapp/RhappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gi.rhapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
